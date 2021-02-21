7e33ea01bd9dc8a47e643cc77d74051311d46ff4
corynorris <corynorris@gmail.com>
2021-02-21 03:32:25 -0500

    Added timer to quit

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -15,7 +15,24 @@ namespace Game.Jam
         [SerializeField] GameObject losePanel;
         [SerializeField] TextMeshProUGUI timeText;
 
+        private Countdown exitCountdown;
+        private GameManager gameManager;
         bool gameOver = false;
+
+        void Awake()
+        {
+            gameManager = FindObjectOfType<GameManager>();
+            exitCountdown = FindObjectOfType<Countdown>();
+        }
+
+        void OnEnable()
+        {
+            exitCountdown.TimesUp += ForceExit;
+        }
+        void OnDisable()
+        {
+            exitCountdown.TimesUp -= ForceExit;
+        }
 
         void Update()
         {
@@ -28,12 +45,20 @@ namespace Game.Jam
         {
             gameOver = true;
             winPanel.SetActive(true);
+            exitCountdown.BeginCountdown();
+
         }
 
         public void Lose()
         {
             gameOver = true;
             losePanel.SetActive(true);
+            exitCountdown.BeginCountdown();
+        }
+
+        public void ForceExit()
+        {
+            gameManager.QuitToMenu();
         }
     }
 }
